4eeed6f453c1178c33bf2f7416276b13cbbbfb07
joaquin4446 <joaquin4446@gmail.com>
2022-11-11 14:07:27 -0400

    first commit

---
+++ prisma/migrations/20221108003028_modificacion/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `productosId` on the `categoria` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE `categoria` DROP COLUMN `productosId`;
